a970f999ac58d1ea4121d807cc46329a8b40b4bf
arislan <arislan87@gmail.com>
2013-08-14 02:20:16 +0300

    2

--- MvcApplication1/MvcApplication1/Controllers/ValuesController.cs
+++ MvcApplication1/MvcApplication1/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace MvcApplication1.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "hahah", "value2" };
         }
 
         // GET api/values/5
